20907822b236d3deba06aeb44d4489b33dec0f60
Lejusdefruits <100711959+Lejusdefruits@users.noreply.github.com>
2025-07-27 21:44:11 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,34 +1,20 @@
 #include "./includes/ft.h"
 
-void	convert_number_to_words(char *input, t_dict *dict, int size)
+static void	print_power(int pos, t_dict *dict, int size)
 {
-	int	len;
-	int	group;
-	int	i;
-	int	chunk_size;
+	char	*pow;
+	char	*label;
 
-	len = ft_strlen(input);
-	if (len == 0)
+	pow = create_power_string(pos);
+	if (!pow)
 		return ;
-	if (get_word_for_number(dict, size, input))
-	{
-		ft_putstr(ft_strcat(get_word_for_number(dict, size, input), "\n"));
-		return ;
-	}
-	group = (len + 2) / 3 - 1;
-	i = 0;
-	while (i < len)
-	{
-		chunk_size = (i == 0 && len % 3) ? len % 3 : 3;
-		process_chunk(input, i, chunk_size, dict, size, group);
-		i += chunk_size;
-		group--;
-	}
-	ft_putstr("\n");
+	label = get_word_for_number(dict, size, pow);
+	if (label)
+		ft_putstr(ft_strcat(label, " "));
+	else
+		break_down_large_power(pow, dict, size);
+	free(pow);
 }
-
-
-#include "./includes/ft.h"
 
 void	process_chunk(char *input, int i, int chunk_size,
 		t_dict *dict, int size, int group)
@@ -49,4 +35,32 @@ void	process_chunk(char *input, int i, int chunk_size,
 		if (group > 0)
 			print_power(group, dict, size);
 	}
+}
+
+void	convert_number_to_words(char *input, t_dict *dict, int size)
+{
+	int	len;
+	int	group;
+	int	i;
+	int	chunk_size;
+
+	len = ft_strlen(input);
+	if (len == 0)
+		return ;
+	if (get_word_for_number(dict, size, input))
+		return (ft_putstr(
+				ft_strcat(get_word_for_number(dict, size, input), "\n")));
+	group = (len + 2) / 3 - 1;
+	i = 0;
+	while (i < len)
+	{
+		if (i == 0 && len % 3)
+			chunk_size = len % 3;
+		else
+			chunk_size = 3;
+		process_chunk(input, i, chunk_size, dict, size, group);
+		i += chunk_size;
+		group--;
+	}
+	ft_putstr("\n");
 }
